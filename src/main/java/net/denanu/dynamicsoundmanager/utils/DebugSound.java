6d775d71c01518f4d0182e966888ca8f92945b18
Julian Wandhoven <Julian.wandhoven@gmail.com>
2023-01-21 23:32:32 +0100

    clean up

--- src/main/java/net/denanu/dynamicsoundmanager/utils/DebugSound.java
+++
@@ -1,9 +0,0 @@
-package net.denanu.dynamicsoundmanager.utils;
-
-import net.denanu.dynamicsoundmanager.DynamicSoundManager;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.util.Identifier;
-
-public class DebugSound {
-	public static SoundEvent TEST = new SoundEvent(Identifier.of(DynamicSoundManager.MOD_ID, "test"));
-}
